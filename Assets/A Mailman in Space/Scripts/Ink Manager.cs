a7be54aa1d3f1c5fbb952629e9f9eb502edaf9e4
Knappkin <44062671+Knappkin@users.noreply.github.com>
2025-03-18 23:39:22 -0400

    Clicking on the line now works.
    Lines with the choice tag are given a link
    
    Currently the link ID is not working as intended, created as blank for some reason.

--- Assets/A Mailman in Space/Scripts/Ink Manager.cs
+++ Assets/A Mailman in Space/Scripts/Ink Manager.cs
@@ -12,9 +12,10 @@ public class InkManager : MonoBehaviour, IPointerClickHandler
    [SerializeField] private TextAsset inkJSON;
 
     public TextMeshProUGUI textBox;
+
+    private Story currentStory;
     private Story story;
 
-    
 
     private void Awake()
     {
@@ -26,22 +27,54 @@ public class InkManager : MonoBehaviour, IPointerClickHandler
     {
         if (story.canContinue)
         {
+            string newLine = story.Continue();
             List<string> tags = story.currentTags;
 
             if (tags.Count > 0 && tags[0] == "choice")
 
             {
                 Debug.Log("We saw the choice");
+                newLine = "<u>" + "<link = \"choizes\">" + newLine + " </link>" + "</u>";
+                Debug.Log(newLine);
 
             }
-       
-            textBox.text += story.Continue();
+
+            textBox.text += newLine;
         }
     }
 
+    public void openLetter(TextAsset inkJSON)
+    {
+
+    }
+
+
     public void OnPointerClick(PointerEventData eventData)
     {
-       
+        Debug.Log("clickin do works");
+       TMP_Text pTextMeshPro = GetComponent<TMP_Text>();
+
+        Vector3 mousePos = new Vector3(eventData.position.x, eventData.position.y, 0);
+
+        int linkIndex = TMP_TextUtilities.FindIntersectingLink(textBox, Input.mousePosition, null);
+
+        if (linkIndex != -1)
+        {
+            Debug.Log(linkIndex);
+        
+            TMP_LinkInfo linkInfo = textBox.textInfo.linkInfo[linkIndex];
+            //Debug.Log(linkInfo.GetLinkText());
+            Debug.Log(linkInfo.GetLinkID());
+            if (linkInfo.GetLinkID() == "")
+            {
+                Debug.Log("holy shit it worked!!!");
+            }
+            else
+            {
+               Debug.Log("nuh-uh");
+            }
+           
+        }
     }
 
 }
